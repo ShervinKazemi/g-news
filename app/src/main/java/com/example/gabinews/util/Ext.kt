59bf42e2b975419b6862fcc081e94db1a286805d
Shervin <github.kazmy2@gmail.com>
2025-01-04 14:51:54 +0330

    feat(feat-homeScreen): add isValid extension function for NewsResponse

--- app/src/main/java/com/example/gabinews/util/Ext.kt
+++ app/src/main/java/com/example/gabinews/util/Ext.kt
@@ -1,8 +1,17 @@
 package com.example.gabinews.util
 
 import android.util.Log
+import com.example.gabinews.model.data.NewsResponse.Article
 import kotlinx.coroutines.CoroutineExceptionHandler
 
 val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
     Log.e("CoroutineException", "Error: ${throwable.message}", throwable)
+}
+
+fun Article.isValid() :Boolean {
+    return this.source.name != "[Removed]" && title.isNotBlank() && content.isNotBlank()
+}
+
+fun List<Article>.filterValidNews(): List<Article> {
+    return this.filter { it.isValid() }
 }
