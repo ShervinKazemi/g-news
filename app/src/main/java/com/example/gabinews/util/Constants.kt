fdbd051a57f8eaf4b27f4eaaa8472fbce66f0047
Shervin <github.kazmy2@gmail.com>
2025-01-04 15:18:39 +0330

    feat(feat-homeScreen): add category

--- app/src/main/java/com/example/gabinews/util/Constants.kt
+++ app/src/main/java/com/example/gabinews/util/Constants.kt
@@ -4,3 +4,34 @@ object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
     const val API_KEY = "X-Api-Key: 077ff9bb1b7b4cb9a09452a4e1cd379f"
 }
+
+val CATEGORY = listOf(
+    Pair(
+        "Technology",
+        "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6cfb470986679.5bb5b7f2e11ad.jpg"
+    ),
+    Pair(
+        "General",
+        "https://icme2019.org/wp-content/uploads/2023/11/print-media-main-1366x744.jpg"
+    ),
+    Pair(
+        "Health",
+        "https://static.vecteezy.com/system/resources/previews/021/599/814/non_2x/healthcare-heart-shape-with-green-cardio-pulse-heartbeat-lone-medical-abstract-background-modern-simple-design-icon-sign-or-logo-design-vector.jpg"
+    ),
+    Pair(
+        "Science",
+        "https://thumbs.dreamstime.com/b/science-background-illustration-scientific-design-flasks-glass-chemistry-physics-elements-generative-ai-271589262.jpg"
+    ),
+    Pair(
+        "Sports",
+        "https://cdn.pixabay.com/photo/2023/04/17/10/31/tennis-7932066_1280.jpg"
+    ),
+    Pair(
+        "Business",
+        "https://www.3csoftware.com/wp-content/uploads/2019/07/iStock-1094465844lg.jpg"
+    ),
+    Pair(
+        "Entertainment",
+        "https://wallpapers.com/images/featured/entertainment-background-gftd7gh987h1ckko.jpg"
+    )
+)
